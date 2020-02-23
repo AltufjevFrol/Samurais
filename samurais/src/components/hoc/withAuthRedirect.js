3fccb49f621c5d9f63558e1ebdab3cc2c7b66459
Frol <artem.mihailovich@gmail.com>
2020-02-23 14:21:19 +0300

    Написал HOC который перенаправляет на страницу логин если нет куки логинизации в запросах

--- samurais/src/components/hoc/withAuthRedirect.js
+++ samurais/src/components/hoc/withAuthRedirect.js
@@ -1,5 +1,33 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import [Redirect] from 'react-route-dom';
+import {Redirect} from 'react-router-dom';
 
-const
+const withRedirectToLogin = (Component) => {
+
+	/*
+	вот эту компоненту обернем коннектом перед тем как вернуть ее
+	return ((props) => {
+		if (!props.isAuth) {
+			return <Redirect to="/login"/>
+		}
+		return (<Component {...props}/>);
+	});
+	*/
+
+	let mapStateToProps = (state)=>{
+		return {
+			isAuth: state.auth.isAuth
+		}
+	};
+
+	return connect(mapStateToProps)(
+		(props) => {
+			if (!props.isAuth) {
+				return <Redirect to="/login"/>
+			}
+			return (<Component {...props}/>);
+		}
+	)
+	};
+
+export default withRedirectToLogin;
