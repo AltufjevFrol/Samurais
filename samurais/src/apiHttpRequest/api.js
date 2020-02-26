3dd8888ddc975e9e6cdf4ae6b4d16437b2f83bed
AltufjevFrol <artem.mihailovich@gmail.com>
2020-02-26 10:36:34 +0300

    Добавил методы получени и установки статуса

--- samurais/src/apiHttpRequest/api.js
+++ samurais/src/apiHttpRequest/api.js
@@ -1,29 +1,35 @@
 import * as axios from 'axios';
 
 const instance = axios.create({
-	withCredentials: true,
-	baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-	headers: {
-		"API-KEY": "911f303f-91db-4c17-9cf0-964e96ae8b8e"
-	}
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "911f303f-91db-4c17-9cf0-964e96ae8b8e"
+    }
 });
 
 const API = {
-	getAuthMe() {
-		return instance.get('https://social-network.samuraijs.com/api/1.0/auth/me').then(responce => responce.data);
-	},
-	getUser(id) {
-		return instance.get(`profile/${id}`).then((responce) => responce.data);
-	},
-	getUsers(pageSize, numPage) {
-		return instance.get(`users?count=${pageSize}&page=${numPage}`).then((responce) => responce.data);
-	},
-	followUser(id) {
-		return instance.post(`follow/${id}`).then((responce) => responce.data);
-	},
-	unFollowUser(id) {
-		return instance.delete(`follow/${id}`).then((responce) => responce.data);
-	},
+    getAuthMe() {
+        return instance.get('https://social-network.samuraijs.com/api/1.0/auth/me').then(responce => responce.data);
+    },
+    getUser(id) {
+        return instance.get(`profile/${id}`).then((responce) => responce.data);
+    },
+    getUsers(pageSize, numPage) {
+        return instance.get(`users?count=${pageSize}&page=${numPage}`).then((responce) => responce.data);
+    },
+    followUser(id) {
+        return instance.post(`follow/${id}`).then((responce) => responce.data);
+    },
+    unFollowUser(id) {
+        return instance.delete(`follow/${id}`).then((responce) => responce.data);
+    },
+    getStatus(id) {
+        return instance.get(`profile/status/${id}`).then((resp) => resp.data);
+    },
+    setStatus(status) {
+        return instance.put('profile/status/', {status}).then((resp)=>resp.data)
+    }
 };
 
 
