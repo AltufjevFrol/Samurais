9f106fe1a48bc2c9e2c539cc77073ea8c896ebe3
AltufjevFrol <artem.mihailovich@gmail.com>
2020-03-02 12:32:15 +0300

    Добавил thunk creater для логинизации в authReduser

--- samurais/src/redux/authReducer.js
+++ samurais/src/redux/authReducer.js
@@ -4,34 +4,50 @@ const SET_USER_DATA = 'SET-USER-DATA';
 
 export const setUserDataCA = (data) => ({type: SET_USER_DATA, data});
 export const authMe = () => {
-	return (dispatch) => {
-		API.getAuthMe().then((resp) => {
-			if (resp.resultCode === 0) {
-				dispatch(setUserDataCA(resp.data));
-			}
-		})
-	}
+    return (dispatch) => {
+        API.getAuthMe().then((resp) => {
+            if (resp.resultCode === 0) {
+                dispatch(setUserDataCA(resp.data));
+            }
+        })
+    }
 
+};
+
+export const loginMe = (logData) => {
+    return (dispatch) => {
+        API.setAuthMe(logData).then((resp) => {
+            console.log(resp);
+            if (resp.resultCode === 0) {
+                /* dispatch(setUserDataCA(resp.data));*/
+                API.getAuthMe().then((resp) => {
+                    if (resp.resultCode === 0) {
+                        dispatch(setUserDataCA(resp.data));
+                    }
+                });
+            }
+        })
+    }
 };
 
 const initialState = {
-	id: null,
-	login: null,
-	email: null,
-	isAuth: false
+    id: null,
+    login: null,
+    email: null,
+    isAuth: false
 };
 
 const authReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case SET_USER_DATA:
-			return {
-				...state,
-				...action.data,
-				isAuth: true,
-			};
-		default:
-			return state;
-	}
+    switch (action.type) {
+        case SET_USER_DATA:
+            return {
+                ...state,
+                ...action.data,
+                isAuth: true,
+            };
+        default:
+            return state;
+    }
 };
 
 export default authReducer;
