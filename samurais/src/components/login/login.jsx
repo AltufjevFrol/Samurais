5d885baa361cd9b410e0d8ffaab0f704e46ddf26
Frol <artem.mihailovich@gmail.com>
2020-03-10 07:18:28 +0300

    прикрутил проверку капчи

--- samurais/src/components/login/login.jsx
+++ samurais/src/components/login/login.jsx
@@ -3,38 +3,79 @@ import styles from './login.module.css';
 import {Field, reduxForm} from 'redux-form';
 import {connect} from "react-redux";
 import {loginMe} from '../../redux/authReducer'
+import {requiredValidator, maxLengthValidatorCreater, patternValidatorCreater} from "../../utils/validators/validators";
+import {Input, INPUT, InputControl} from "../common/inputsControls";
 
+let maxLengthValidator = maxLengthValidatorCreater(20);
+const regExpEmail = /\S+@\S+\.\S+/;
+let patternValidator = patternValidatorCreater(regExpEmail);
 
 let LoginForm = (props) => {
-    return (
-        <form onSubmit={props.handleSubmit}>
-            <div><Field component={'input'} name={'email'} placeholder={'Name'}/></div>
-            <div><Field component={'input'} name={'password'} placeholder={'password'}/></div>
-            <div><label><Field component={'input'} name={'rememberMe'} type={'checkbox'}/> remember me </label></div>
-            <div>
-                <button >Login</button>
-            </div>
-        </form>
-    )
-};
+
+	let errorBlock = null;
+	if (props.error) {
+		errorBlock = (
+			<div><span>{props.error.value}</span></div>
+		);
+	}
+	if (props.withCapcha) {
+		errorBlock = (
+			<div>
+				<div><img src={props.capchaUrl} alt={'capcha'}/></div>
+				<div><span>Text from capcha</span> <Field component={'input'} name={'captcha'}/></div>
+			</div>
+		);
+	}
+
+
+return (
+	<form onSubmit={props.handleSubmit}>
+		<div>
+			<Field
+				component={InputControl}
+				name={'email'}
+				placeholder={'email'}
+				validate={[requiredValidator, maxLengthValidator, patternValidator]}
+			/>
+		</div>
+		<div>
+			<Field
+				component={InputControl}
+				name={'password'}
+				type={'password'}
+				placeholder={'password'}
+				validate={[requiredValidator, maxLengthValidator]}
+			/>
+		</div>
+		<div><label><Field component={'input'} name={'rememberMe'} type={'checkbox'}/> remember me </label></div>
+		{errorBlock}
+		<div>
+			<button>Login</button>
+		</div>
+	</form>
+)
+}
+;
 
 LoginForm = reduxForm({form: 'login'})(LoginForm);
 
 const Login = (props) => {
-    const onSubmit = (formData)=>{
-        console.log(formData);
-        props.loginMe(formData);
-    }
-    return (
-        <div className={styles.login}>
-            <h1>LOGIN</h1>
-            < LoginForm onSubmit={onSubmit}/>
-        </div>
-    );
+	const onSubmit = (formData) => {
+		console.log(formData);
+		props.loginMe(formData);
+	};
+	return (
+		<div className={styles.login}>
+			<h1>LOGIN</h1>
+			< LoginForm onSubmit={onSubmit} {...props}/>
+		</div>
+	);
 };
 
-const mapDespatchToProps = (state)=>({
-   auth: state.auth,
+const mapDespatchToProps = (state) => ({
+	auth: state.auth,
+	withCapcha: state.auth.withCapcha,
+	capchaUrl: state.auth.capchaUrl
 });
 
-export default connect(mapDespatchToProps,{loginMe})(Login);
+export default connect(mapDespatchToProps, {loginMe})(Login);
