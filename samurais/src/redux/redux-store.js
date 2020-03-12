49fdb069c2b99bc7c23b36c52a02c0aab808f730
Frol <artem.mihailovich@gmail.com>
2020-03-12 07:15:32 +0300

    Появился новый редюсер для всего App

--- samurais/src/redux/redux-store.js
+++ samurais/src/redux/redux-store.js
@@ -8,6 +8,7 @@ import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
 
 import {reducer as formReducer} from 'redux-form';
+import appReducer from "./appReducer";
 
 let reducers = combineReducers({
 	header: headerReducer,
@@ -16,7 +17,9 @@ let reducers = combineReducers({
 	dialogsPage: dialogsReducer,
 	usersPage: usersReducer,
 	auth: authReducer,
-	form: formReducer
+	form: formReducer,
+	app: appReducer
+
 });
 const store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
