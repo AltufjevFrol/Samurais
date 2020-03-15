320f770cbcea2d4ca41f6fc3612bb73b8d69a949
Frol <artem.mihailovich@gmail.com>
2020-03-15 10:01:23 +0300

    установил периодический диспатч тестового экшена

--- samurais/src/index.js
+++ samurais/src/index.js
@@ -8,6 +8,11 @@ import store from './redux/redux-store';
 /*import Context, {Provider} from './context';*/
 import {Provider} from 'react-redux'
 
+setInterval (()=> {
+	store.dispatch({type: 'TEST ACTION'});
+	console.log('trow TEST ACTION')
+}, 1000);
+
 ReactDOM.render(
 	<BrowserRouter >
 		<Provider store={store}>
