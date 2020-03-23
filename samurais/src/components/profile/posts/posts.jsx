ee9968196839d797e2098e7b14b8181f16cdf8f8
Frol <artem.mihailovich@gmail.com>
2020-03-23 05:56:32 +0300

    Перешел на классовый компонент, что бы заюзать shouldComponentUpdate, оказалось компонента и так не обновлялась. Не понятно почему она обновлялась у Димыча

--- samurais/src/components/profile/posts/posts.jsx
+++ samurais/src/components/profile/posts/posts.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styles from './posts.module.css';
 import Post from './post/post';
 import Sender from '../../sender/sender';
 import {addLikePostCreateAction, addPostCreateAction} from "../../../redux/profileReducer";
 
 
-function Posts(props) {
+class Posts extends Component {
 
-	/*let addPost = props.addPost;
-	let addNewSymbolPost = props.addNewSymbolPost;
-	let addLike = props.addLike;*/
+	shouldComponentUpdate(nextProps, nextState, nextContext) {
+		return true;
+	}
 
-	let posts = props.postsData.map((post) => (
-		<Post key={post.id} post={post} likes={post.like} hendlerLike={props.addLikePostCreateAction}/>));
-
-	return (
-		<div className={styles.blok_posts}>
-			<h1>My posts</h1>
-			<Sender hendlerSend={props.addPostCreateAction} hendlerInput={props.addNewSymbolPostCreateAction}/>
-			{posts}
-		</div>
-	)
+	render() {
+		let posts = this.props.postsData.map((post) => (
+			<Post key={post.id} post={post} likes={post.like} hendlerLike={this.props.addLikePostCreateAction}/>));
+		console.log('RENDER Posts');
+		console.log(this.props);
+		return (
+			<div className={styles.blok_posts}>
+				<h1>My posts</h1>
+				<Sender hendlerSend={this.props.addPostCreateAction} hendlerInput={this.props.addNewSymbolPostCreateAction}/>
+				{posts}
+			</div>
+		)
+	}
 }
 
 export default Posts;
+
