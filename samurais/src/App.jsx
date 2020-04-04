15d78052df85e319bdd42211e58c6712e39ec9fa
Frol <artem.mihailovich@gmail.com>
2020-04-04 11:20:34 +0300

    Попробовал React.lazy и динамический импорт

--- samurais/src/App.jsx
+++ samurais/src/App.jsx
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './App.css';
 import HeaderContainer from './components/header/headerContainer.js';
 import Navbar from './components/navbar/navbar.jsx';
 import {BrowserRouter, Route} from 'react-router-dom';
-import DialogsContainer from "./components/dialogs/dialogsContainer";
-import UsersContainer from "./components/users/usersContainer";
-import ProfileContainer from "./components/profile/profileContainer";
+/*import DialogsContainer from "./components/dialogs/dialogsContainer";*/
+/*import UsersContainer from "./components/users/usersContainer";*/
 import Login from "./components/login/login";
+import ProfileContainer from "./components/profile/profileContainer";
 import {initializeApp} from "./redux/appReducer";
 import {Provider, connect} from "react-redux";
 import store from "./redux/redux-store";
 
+const DialogsContainer = React.lazy(() => import('./components/dialogs/dialogsContainer'));
+import('./components/users/testDinamicImport.js').then((TDI)=>{
+	console.log(TDI);
+	TDI.func(TDI.data);
+	TDI.setData ('new data');
+	TDI.func(TDI.data);
+	TDI.default();
+});
+const UsersContainer = React.lazy(()=>import("./components/users/usersContainer"));
+
+/*const UsersContainer
+const Login*/
+
+
 function App(props) {
 
 	return (
-		< div className="app-wraper">
-			< HeaderContainer/>
-			< Navbar/>
-			< div className="app-wraper-content">
-				<Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
-				<Route path="/dialogs" component={DialogsContainer}/>
-				<Route path="/users" component={UsersContainer}/>
-				<Route path="/login" component={Login}/>
+		<Suspense fallback={<div style={{background: 'red'}}>Please waiting</div>}>
+			< div className="app-wraper">
+				< HeaderContainer/>
+				< Navbar/>
+
+				<div className="app-wraper-content">
+					<Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
+					<Route path="/dialogs" component={DialogsContainer}/>
+					<Route path="/users" component={UsersContainer}/>
+					<Route path="/login" component={Login}/>
+				</div>
+
 			</div>
-		</div>
+		</Suspense>
 	);
 }
 
